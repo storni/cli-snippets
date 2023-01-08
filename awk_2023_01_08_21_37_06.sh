4b66d41ffdd08baf25a7a7eea68bd6ec5bf33e94
Javier Storni <storni@gmail.com>
2023-01-08 18:41:56 -0300

    reset

--- awk_2023_01_08_21_37_06.sh
+++
@@ -1,6 +0,0 @@
-Twit Format:
-Need to quickly convert a text file? Use the Awk command! Sample command: awk '{print $1,$3}' file.txt #Linux #Bash #CommandLine
-
-# Awk is a powerful command line tool that can be used to quickly convert a text file into a different format.
-# In this example, we use the awk command to print the first and third columns from a text file.
-awk '{print $1,$3}' file.txt
