7330b72ededda8a0651df7927300e64f64071a0e
Javier Storni <storni@gmail.com>
2023-01-08 18:37:35 -0300

    Adding a new script of tool cut

---
+++ cut_2023_01_08_21_37_35.sh
@@ -0,0 +1,7 @@
+Twit Format:
+Cut command line tool to quickly extract columns from a text file: cut -f 2,3 -d ':' input.txt #linux #commandline
+
+# The cut command line tool is used to quickly extract columns from a text file
+# -f defines which fields to include in the output
+# -d defines the delimiter character used to separate the fields
+cut -f 2,3 -d ':' input.txt
