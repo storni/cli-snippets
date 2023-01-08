356a42d9812e5c16ea9c388a3a85b92fe477f9d5
Javier Storni <storni@gmail.com>
2023-01-08 18:46:32 -0300

    Adding a new script of tool wc

---
+++ wc/2023_01_08_21_46_32.sh
@@ -0,0 +1,6 @@
+Twit Format:
+Did you know you can use the WC command in Linux? It's a great way to count the number of words, lines, and characters in a file! Just run `wc <filename>` to get started. #Linux #CommandLine #WC
+
+# Use the WC command to count the number of words, lines, and characters in a file
+wc <filename>
+# The output is the number of lines, words, and characters in the file, in that order.
